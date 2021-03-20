8258c489575924a1a65e1411f2a3c283bc599b07
高源 <gaoyuan@gaoyuandeMacBook-Pro.local>
2021-03-20 18:48:13 +0900

    add the posts and hobbies query

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -107,6 +107,12 @@ const RootQuery = new GraphQLObjectType({
 
       }
     },
+    users: {
+      type: new graphql.GraphQLList(UserType),
+      resolve(parent, args) {
+        return usersData
+      }
+    },
     hobby: {
       type: HobbyType,
       args:{id:{type:GraphQLID}},
@@ -114,11 +120,23 @@ const RootQuery = new GraphQLObjectType({
         return _.find(hobbiesData, {id: args.id})
       }
     },
+    hobbies: {
+      type: new graphql.GraphQLList(HobbyType),
+      resolve(parent, args) {
+        return hobbiesData
+      }
+    },
     post: {
       type: PostType,
       args: {id:{type:GraphQLID}},
       resolve(parent, args) {
         return _.find(postsData, {id: args.id})
+      }
+    },
+    posts: {
+      type: PostType,
+      resolve(parent, args) {
+        return postsData
       }
     }
   }
